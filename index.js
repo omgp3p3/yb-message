6278ac3c5a70c41c31b9a2290211fd9bd588a2d1
cozups <kimms9725@naver.com>
2022-05-31 05:51:11 +0900

    0517~0529

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 import privateMessage from './message.js';
 
+let nickname = '닉네임';
 const messageList = document.getElementById('messages');
 
 function renderMessage() {
+  let aHtml = [];
   privateMessage.forEach((day) => {
-    console.log(day);
     const date = day.date;
     const messages = day.messages;
 
-    messageList.innerHTML += `
-      <div class="message-date">${date}</div>
-    `;
+    aHtml.push(`<div class="message-date">${date}</div>`);
 
     messages.forEach((msg) => {
-      const element = `
+      msg.content = msg.content.replace('@@', nickname);
+      let element = `
       <li>
         <div class="profile">
           <img src="./images/profile.jpg" class="profile-image" />
@@ -27,8 +27,25 @@ function renderMessage() {
       </li>
       `;
 
-      messageList.innerHTML += element;
+      if (msg.content.indexOf('images') >= 0) {
+        element = `
+        <li>
+          <div class="profile">
+            <img src="./images/profile.jpg" class="profile-image" />
+            <span class="name">유빈</span>
+          </div>
+          <div class="artist-message">
+            <a href="${msg.content}" target="_blank"><img src="${msg.content}" class="message-photo"/></a>
+            <span class="date">${date}</span>
+            <span class="time">${msg.time}</span>
+          </div>
+        </li>
+        `;
+      }
+
+      aHtml.push(element);
     });
+    messageList.innerHTML = aHtml.join('');
   });
 }
 renderMessage();
